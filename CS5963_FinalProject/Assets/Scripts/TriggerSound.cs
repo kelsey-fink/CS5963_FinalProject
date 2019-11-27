8f39ccf83630e9db81eebcf42cbef17617e273ac
Leo <u0986505@utah.edu>
2019-11-27 15:25:53 -0700

    Added two scripts for sound and effect triggers

---
+++ CS5963_FinalProject/Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour {
+
+    public GameObject trigger;
+    public AudioSource audioClip;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            trigger.SetActive(true); //maybe not necessary
+            audioClip.Play();
+        }
+    }
+}
